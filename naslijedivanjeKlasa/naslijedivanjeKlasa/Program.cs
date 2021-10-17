e6291f239ddd6f135e6b9f53a2e5b18b766c286a
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-10-17 14:15:05 +0200

    Add files via upload

---
+++ naslijedivanjeKlasa/naslijedivanjeKlasa/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace naslijedivanjeKlasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bjelogoricno hrast = new Bjelogoricno(true);
+            Crnogoricno bor = new Crnogoricno(false);
+
+
+            Console.WriteLine("Hrast je "+hrast.OpadajuListovi+", bor je " + bor.OpadajuListovi);
+           
+        }
+      
+    }
+}
